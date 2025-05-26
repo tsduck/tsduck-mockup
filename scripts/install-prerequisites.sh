a06389c3be3e8c4af82431f6c25753009e734e67
Thierry Lelegard <thierry@lelegard.fr>
2025-05-26 22:26:45 +0200

    Update

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -234,7 +234,7 @@ elif [[ "$SYSTEM" == "NetBSD" ]]; then
 
 elif [[ "$DISTRO" == "Ubuntu" ]]; then
 
-    PKGLIST+=(git g++ make cmake flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
+    PKGLIST+=(git g++ make cmake locales-all flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
     [[ -z $NOOPENSSL                                       ]] && PKGLIST+=(libssl-dev)
     [[ -z $NOEDITLINE                                      ]] && PKGLIST+=(libedit-dev)
     [[ -z $NOPCSC                                          ]] && PKGLIST+=(pcscd libpcsclite-dev)
@@ -270,7 +270,7 @@ elif [[ "$DISTRO" == "Ubuntu" ]]; then
 
 elif [[ "$DISTRO" == "Linuxmint" ]]; then
 
-    PKGLIST+=(git g++ make cmake flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
+    PKGLIST+=(git g++ make cmake locales-all flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
     [[ -z $NOOPENSSL                               ]] && PKGLIST+=(libssl-dev)
     [[ -z $NOEDITLINE                              ]] && PKGLIST+=(libedit-dev)
     [[ -z $NOZLIB                                  ]] && PKGLIST+=(zlib1g-dev)
@@ -307,7 +307,7 @@ elif [[ "$DISTRO" = "Debian" || "$DISTRO" = "Raspbian" ]]; then
     [[ -z $MAJOR ]] && lsb_release -d | grep -qi duke && MAJOR=15
     [[ -z $MAJOR ]] && MAJOR=99
 
-    PKGLIST+=(git g++ make cmake flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
+    PKGLIST+=(git g++ make cmake locales-all flex bison dos2unix curl tar zip linux-libc-dev dpkg-dev python3)
     [[ -z $NOOPENSSL               ]] && PKGLIST+=(libssl-dev)
     [[ -z $NOEDITLINE              ]] && PKGLIST+=(libedit-dev)
     [[ -z $NOZLIB                  ]] && PKGLIST+=(zlib1g-dev)
